812cc5f462456cd23e0ec7c381c245860f65208b
kerley <kerley.kls@gmail.com>
2022-07-29 21:04:34 -0300

    correcting the name of env conecctions

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -4,10 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
 });
 
 export default connection;
